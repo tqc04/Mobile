1c58f86f55510b3d513fd1ba63444989199ebf0b
NGUYEN HOAI KHUONG <22130128@st.hcmuaf.edu.vn>
2025-06-05 14:34:07 +0700

    test

--- app/src/main/java/com/example/finalapp/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/finalapp/ui/search/SearchFragment.java
@@ -335,6 +335,7 @@ public class SearchFragment extends Fragment {
             }
             @Override
             public void onCancelled(@NonNull DatabaseError error) {}
+
         });
     }
 }
